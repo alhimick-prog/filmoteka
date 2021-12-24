634a512b969522e7d5219689e1d2fd6675615aa2
alhimick-prog <alhimick96@yahoo.com>
2021-12-24 18:53:16 +0300

    Small style fixes in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@
 #
 class Seed
   def self.reset_database_before_seeding
-    puts "========== Reset DB before seeding ========="
+    puts '========== Reset DB before seeding ========='
     require 'rake'
     Rake::Task['db:reset'].invoke
     Rake::Task['db:create'].invoke
     Rake::Task['db:migrate'].invoke
   end
 
-  def self.seed_users(count=1)
+  def self.seed_users(count = 1)
     puts "========== Seed #{count} users =========="
     count.times do
       User.create!(email: Faker::Internet.email,
@@ -21,7 +21,7 @@ class Seed
     end
   end
 
-  def self.seed_films(count=1)
+  def self.seed_films(count = 1)
     puts "========== Seed #{count} films =========="
     count.times do
       Film.create!(title: Faker::Movie.title,
@@ -34,27 +34,27 @@ class Seed
   end
 
   def self.seed_categories
-    puts "========== Seed categories =========="
+    puts '========== Seed categories =========='
     ['Family', 'Horror', 'Male', "Children's", 'Adult', 'Soap opera'].each do |category_name|
       Category.create!(name: category_name)
     end
   end
 
   def self.seed_countries
-    puts "========== Seed countries =========="
+    puts '========== Seed countries =========='
     ISO3166::Country.translations(:en).each do |symb, country_name|
       Country.create!(name: country_name, banner: ISO3166::Country(symb).emoji_flag)
     end
   end
 
   def self.seed_genres
-    puts "========== Seed genres =========="
+    puts '========== Seed genres =========='
     ['Action movie', 'Western', 'Detective', 'Comedy', 'Drama', 'Melodrama', 'Noir', 'Musical'].each do |genre|
       Genre.create!(name: genre)
     end
   end
 
-  def self.seed_people(count=1)
+  def self.seed_people(count = 1)
     puts "========== Seed #{count} people =========="
     count.times do
       birth = Faker::Date.between(from: '1800-01-01', to: Date.today)
@@ -62,20 +62,18 @@ class Seed
       death = if Faker::Boolean.boolean(true_ratio: 0.3)
         Faker::Date.between(from: min_death_date, to: Date.today)
       end
-      Person.create!(name: Faker::Name.name,
-                     birthday: birth,
-                     death_date: death)
+      Person.create!(name: Faker::Name.name, birthday: birth, death_date: death)
     end
   end
 
-  def self.seed_tags(count=1)
+  def self.seed_tags(count = 1)
     puts "========== Seed #{count} tags =========="
     count.times do
       Tag.create!(name: Faker::Lorem.word)
     end
   end
 
-  def self.seed_comments(count_to_films=0, count_to_comments=0)
+  def self.seed_comments(count_to_films = 0, count_to_comments = 0)
     puts "========== Seed #{count_to_films} comments to films =========="
     count_to_films.times do
       Comment.create!(commentable_id: Film.ids.sample,
@@ -92,7 +90,7 @@ class Seed
     end
   end
 
-  def self.seed_seasons(count=1)
+  def self.seed_seasons(count = 1)
     puts "========== Seed #{count} seasons =========="
     count.times do
       film = Film.all.sample
@@ -103,7 +101,7 @@ class Seed
     end
   end
 
-  def self.seed_episodes(seasons_numb=1, min=5, max=15)
+  def self.seed_episodes(seasons_numb = 1, min = 5, max = 15)
     puts "========== Seed from #{min} to #{max} episodes for #{seasons_numb} seasons =========="
     seasons_numb.times do
       season = Season.all.sample
@@ -115,54 +113,54 @@ class Seed
   end
 
   def self.seed_f_categories
-    puts "========== Seed film_categories =========="
+    puts '========== Seed film_categories =========='
     Film.all.each do |film|
       film.film_categories.create!(category_id: Category.ids.sample)
     end
   end
 
   def self.seed_f_countries
-    puts "========== Seed film_countries =========="
+    puts '========== Seed film_countries =========='
     Film.all.each do |film|
       film.film_countries.create!(country_id: Country.ids.sample)
     end
   end
 
   def self.seed_f_genres
-    puts "========== Seed film_genres =========="
+    puts '========== Seed film_genres =========='
     Film.all.each do |film|
       film.film_genres.create!(genre_id: Genre.ids.sample)
     end
   end
 
   def self.seed_f_people
-    puts "========== Seed film_people =========="
+    puts '========== Seed film_people =========='
     Film.all.each do |film|
       film.film_people.create!(person_id: Person.ids.sample, role: 'Actor')
-      film.film_people.create!(person_id: Person.ids.sample, role:'Director')
+      film.film_people.create!(person_id: Person.ids.sample, role: 'Director')
     end
   end
 
   def self.seed_f_tags
-    puts "========== Seed film_tags =========="
+    puts '========== Seed film_tags =========='
     Film.all.each do |film|
       film.film_tags.create!(tag_id: Tag.ids.sample)
     end
   end
 
   def self.seed_watch_items
-    puts "========== Seed watch_items =========="
+    puts '========== Seed watch_items =========='
     Film.all.each do |film|
       film.watch_items.create!(user_id: User.ids.sample, status: [0, 1, 2].sample)
     end
   end
-  
-  def self.seed_editor_role_and_add_film_creator(count_creators=1, count_films=1)
+
+  def self.seed_editor_role_and_add_film_creator(count_creators = 1, count_films = 1)
     puts "========== #{count_creators} users to editors, #{count_films} films add creators =========="
     count_creators.times do
-      User.all.sample.update!(role:'editor')
+      User.all.sample.update!(role: 'editor')
     end
-    count_films.times do |object_id|
+    count_films.times do
       Film.all.sample.update!(creator_id: User.where(role: 'editor').ids.sample)
     end
   end
@@ -178,11 +176,11 @@ Seed.seed_people(100)
 Seed.seed_tags(50)
 Seed.seed_comments(200, 50)
 Seed.seed_seasons(10)
-Seed.seed_episodes(seasons_numb=10, min=3, max=12)
+Seed.seed_episodes(seasons_numb = 10, min = 3, max = 12)
 Seed.seed_f_categories
 Seed.seed_f_countries
 Seed.seed_f_genres
 Seed.seed_f_people
 Seed.seed_f_tags
 Seed.seed_watch_items
-Seed.seed_editor_role_and_add_film_creator(count_creators=10, count_films=100)
+Seed.seed_editor_role_and_add_film_creator(count_creators = 10, count_films = 100)
