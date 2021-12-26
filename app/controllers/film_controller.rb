1bc7adfd314e12d1528d3ddcfd1a854d1112180a
alhimick-prog <alhimick96@yahoo.com>
2021-12-26 19:29:52 +0300

    Action show

---
+++ app/controllers/film_controller.rb
@@ -0,0 +1,14 @@
+class FilmController < ApplicationController
+  def index
+    #@page = params[:id]
+    @films = Film.all
+  end
+  def show
+    @film = Film.find(params[:id])
+  end
+  def new; end
+  def create; end
+  def edit; end
+  def update; end
+  def destroy; end
+end
