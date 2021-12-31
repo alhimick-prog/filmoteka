2529c19c2afca87af9ca7494ac738658b5b4f73f
alhimick-prog <alhimick96@yahoo.com>
2021-12-31 15:40:13 +0300

    category_controller

--- app/models/film.rb
+++ app/models/film.rb
@@ -43,4 +43,6 @@ class Film < ApplicationRecord
   validates :age_restriction,
             presence: true,
             numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 21 }
+  
+  self.per_page = 15
 end
