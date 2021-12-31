5d76197b601ea49027e25ee94b7aa307961409f9
alhimick-prog <alhimick96@yahoo.com>
2021-12-31 12:41:25 +0300

    films_controllers, gem will_paginate

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,49 @@
+class FilmsController < ApplicationController
+  def index
+    @films = Film.paginate(page: params[:page])
+  end
+  
+  def show
+    @film = Film.find(params[:id])
+  end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    @film = Film.new(film_params)
+
+    if @film.save
+      redirect_to @film
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  def update
+    @film = Film.find(params[:id])
+
+    if @film.update(film_params)
+      redirect_to @film
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @film = Film.find(params[:id])
+    @film.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def film_params
+    params.require(:film).permit(:title, :description, :trailer_url, :release_date, :duration, :age_restriction, :creator_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :film
+  resources :films
 end
